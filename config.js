6681fe96d9baea2edbe5d3c9170890afd940992c
Richard-Thompson <richardthompson1990@hotmail.co.uk>
2017-05-19 18:09:25 +0100

    testing

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
   DB: {
     test: 'mongodb://localhost/northcoders-news-api-test',
-    dev: 'mongodb://Richard-Thompson:dlo19589@ds129179.mlab.com:29179/north_coders_news'
+    dev: 'mongodb://Richard-Thompson:dlo19589@ds129179.mlab.com:29179/north_coders_news',
+    node:''
   },
   PORT: {
     test: 3090,
